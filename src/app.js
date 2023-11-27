5582f429c0a3b5eed9b58cd90b4524893c5dc3d1
BautistaGaber <bautistagaber@gmail.com>
2023-11-27 01:50:16 -0300

    add app.js

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express, { urlencoded } from 'express'
+import  { productsRouter } from './routes/products.js'
+import { cartsRouter } from './routes/carts.js'
+
+
+const PORT = 8080
+const app = express()
+
+app.use(express.json())
+app.use(urlencoded({ extended: true }))
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port: ${PORT}`)
+})
+
+app.use('/api/products', productsRouter)
+app.use('/api/carts', cartsRouter)
