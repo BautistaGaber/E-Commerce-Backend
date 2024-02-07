cbd558ffa01a75cc77c0c99dabbc8d363ca243a1
BautistaGaber <bautistagaber@gmail.com>
2024-02-07 17:31:54 -0300

    add controller file

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,16 +1,13 @@
 import { Router } from "express";
-import userModel from "../dao/fileSystem/mongodb/models/user.model.js";
-import { createHash, validatePassword } from "../utils.js";
 import passport from "passport";
+import { UserController } from "../controller/user.controller.js";
 
 const router = new Router();
 
 router.post(
   "/register",
   passport.authenticate("register", { failureRedirect: "" }),
-  async (req, res) => {
-    res.send({ status: "success", message: "User registered successfully" });
-  }
+  UserController.register
 );
 
 router.get("/failregister", async (req, res) => {
@@ -20,20 +17,7 @@ router.get("/failregister", async (req, res) => {
 router.post(
   "login",
   passport.authenticate("login", { failureRedirect: "" }),
-  async (req, res) => {
-    if (!req.user) {
-      return res.status(400).send({
-        status: "Error",
-        error: "Datos incorrectos",
-      });
-    }
-    req.session.user = {
-      fullName: `${req.user.firstName} ${req.user.lastName}`,
-      email: req.user.email,
-      age: req.user.age,
-    };
-    res.send({ status: "success", payload: req.session.user });
-  }
+  UserController.login
 );
 
 router.get("/faillogin", (req, res) => {
@@ -46,22 +30,10 @@ router.get(
   async (req, res) => {}
 );
 
-// router.get(
-//   "/githubcallback",
-//   passport.authenticate("github", { failureRedirect: "/login" }),
-//   async (req, res) => {
-//     req.session.user = req.user;
-//     res.redirect("/");
-//   }
-// );
-
 router.get(
-  '/githubcallback',
-  passport.authenticate('github', { scope: ['user:email'], sesion: false }),
-  async (req, res) => {
-    console.log(req.user)
-    res.redirect('/views/home')
-  }
-)
+  "/githubcallback",
+  passport.authenticate("github", { scope: ["user:email"], sesion: false }),
+  UserController.gitHubCallback
+);
 
 export { router as userRouter };
