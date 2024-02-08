5ddce5eb737b5ec9e67a37ebcecc42ee4b97ae8c
BautistaGaber <bautistagaber@gmail.com>
2024-02-08 16:02:40 -0300

    .env file added

--- src/app.js
+++ src/app.js
@@ -14,23 +14,21 @@ import session from "express-session";
 import MongoStore from "connect-mongo";
 import inicializePassport from "./config/passport.config.js";
 import passport from "passport";
+import 'dotenv/config';
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 
-const MONGO =
-  "mongodb+srv://bautistagaber:UZ7N4k7m88BIAs2L@cluster0.hdnj5k1.mongodb.net/segunda-preentrega";
-
-const connection = mongoose.connect(MONGO);
+const connection = mongoose.connect(process.env.MONGO_URL);
 
 app.use(
   session({
     store: new MongoStore({
-      mongoUrl: MONGO,
+      mongoUrl: process.env.MONGO_URL,
     }),
     secret: "1234",
     resave: false,
